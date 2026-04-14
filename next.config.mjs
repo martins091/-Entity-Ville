c74efd7b2a5c07b55a44fe84bd9da171f5109cdb
martins091 <agbomartinsejiofor@gmail.com>
2026-04-14 14:00:05 +0100

    updated to static

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 👈 THIS is required for cPanel static hosting
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
-
+export default nextConfig;
